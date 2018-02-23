9a73960ecea37e20f74606c5fae0e7204c596e6e
mineral99 <35477617+mineral99@users.noreply.github.com>
2018-02-23 11:13:12 +0000

    Update webhooks.php

--- webhooks.php
+++ webhooks.php
@@ -3,7 +3,7 @@
 require "vendor/autoload.php";
 require_once('vendor/linecorp/line-bot-sdk/line-bot-sdk-tiny/LINEBotTiny.php');
 
-$access_token = '3ALKAbKFoGuJyJnoDdn0HeyfbxLFtEXBKiC0lFeoNl/XbL4WhoCZzefp2n7UDuXaCWfErIDro07BnZNggJmXJChXTIlMPo8LRJ+n1LEgbRUaKehDkiCr5p5CakHrPX+gauOGX/R5bB2e5yi7xjnHDAdB04t89/1O/w1cDnyilFU=';
+$access_token = 'FprsQKHLjqJPX4TkZzaQa2jAz1MHI48/KZtN9ywW67R6RnOVg4mQGROXRu3Bp+AmYQmuu153s5xl0G33mND4JzCmKBw4PoPn28rAjUnTIWYu3Q8BApDeqD0Fu71VM6McvkJujw7iiw9IOWzQEVNGswdB04t89/1O/w1cDnyilFU=';
 
 // Get POST body content
 $content = file_get_contents('php://input');
